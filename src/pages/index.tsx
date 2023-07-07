9621b28ea6ffceb6da953ec279a55da919aaf938
Prabin Parajuli <prabinparajuli92@gmail.com>
2023-07-08 00:36:22 +0545

    Remove antdesign config provider content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,19 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: "#fb6e67",
-        },
-        components: {
-          Button: {
-            margin: 10,
-            borderRadius: 999,
-          },
-        },
-      }}
-    >
+    <ConfigProvider>
       <div className="app font-primary">
         <HeroSection />
       </div>
